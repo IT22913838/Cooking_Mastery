0c28db4909b4da019b5f5ac273e700dba392d7c2
IT22913838 <IT22913838@my.sliit.lk>
2025-03-26 14:21:40 +0530

    Creating Spring Boot Maven Project

---
+++ cooking_mastery_api/src/test/java/com/cooking_mastery/cooking_mastery_api/CookingMasteryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooking_mastery.cooking_mastery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingMasteryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
