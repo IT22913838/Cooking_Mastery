3cd6b9c337df1f7dc37c63384cf45537a97a1f3c
Uwasara Indumini <106859423+IT22913838@users.noreply.github.com>
2025-04-28 22:14:16 +0530

    Revert "Merge backend feature implementations into main (#2)" (#3)
    This reverts commit 335546d00fccb83f088dbbe16fb57e83f15df528.

---
+++ cooking_mastery_api/src/main/java/com/cooking_mastery/cooking_mastery_api/CookingMasteryApiApplication.java
@@ -0,0 +1,13 @@
+package com.cooking_mastery.cooking_mastery_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingMasteryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingMasteryApiApplication.class, args);
+	}
+
+}
